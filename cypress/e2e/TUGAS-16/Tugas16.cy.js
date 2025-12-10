102c6e4d01f7d618e81c8c12ade5602a2497f4f1
gunawanchandro1 <gunawanchandro1@gmail.com>
2025-12-10 22:20:57 +0700

    Tugas 16

--- cypress/e2e/TUGAS-16/Tugas16.cy.js
+++ cypress/e2e/TUGAS-16/Tugas16.cy.js
@@ -8,9 +8,14 @@ it('TC01 - Login dengan username valid & password valid', () => {
         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
             .type('admin123')
 
+        cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/dashboard/employees/action-summary')
+        .as('actionSumarry')    
+
         cy.get('button[type="submit"]')
             .should('be.visible')
             .click()
+
+        cy.wait('@actionSumarry').its('response.statusCode').should('eq',200)
 
         cy.url().should('include', '/dashboard')
     })
@@ -25,7 +30,12 @@ it('TC01 - Login dengan username valid & password valid', () => {
         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
             .type('admin123')
 
+        cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages')
+            .as('messages')
+
         cy.get('button[type="submit"]').click()
+
+        cy.wait('@messages').its('response.statusCode').should('eq',304)
 
         cy.get('.oxd-alert-content')
             .should('contain', 'Invalid credentials')
@@ -40,7 +50,12 @@ it('TC01 - Login dengan username valid & password valid', () => {
         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
             .type('admin')
 
+        cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages')
+            .as('messages')
+
         cy.get('button[type="submit"]').click()
+
+        cy.wait('@messages').its('response.statusCode').should('eq',304)
 
         cy.get('.oxd-alert-content')
             .should('contain', 'Invalid credentials')
@@ -49,8 +64,14 @@ it('TC01 - Login dengan username valid & password valid', () => {
     it('TC04 - Login dengan field kosong', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
+ 
+        // cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/dist/css/chunk-vendors.css?v=1721393199309')
+        //     .as('messages')
 
         cy.get('button[type="submit"]').click()
+
+        // cy.wait('@messages').its('response.statusCode').should('eq',200)
+
 
         cy.get(':nth-child(2) > .oxd-input-group > .oxd-text')
             .should('contain', 'Required')
@@ -67,7 +88,12 @@ it('TC01 - Login dengan username valid & password valid', () => {
         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
             .type('admin123')
 
+        cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages')
+            .as('messages')
+
         cy.get('button[type="submit"]').click()
+
+        cy.wait('@messages').its('response.statusCode').should('eq',304)
 
         cy.get('.oxd-alert-content-text')
             .should('contain', 'Invalid credentials')
